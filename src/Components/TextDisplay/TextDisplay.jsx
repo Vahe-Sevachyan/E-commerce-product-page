b8806ca2a8988cc13bfa49d2b6fbbe2462660e86
Vahe Sevachyan <vscode13@gmail.com>
2023-11-20 20:48:24 -0800

    updated text display styles

--- src/Components/TextDisplay/TextDisplay.jsx
+++ src/Components/TextDisplay/TextDisplay.jsx
@@ -2,14 +2,18 @@ import "./TextDisplay.css";
 const TextDisplay = () => {
   return (
     <section className="text-display-container">
-      <h3>SNEAKER COMPANY</h3>
-      <h3>Fall Limited Edition Sneakers</h3>
-      <p>
+      <p className="sneaker-header">SNEAKER COMPANY</p>
+      <h3 className="limited-edition-title">Fall Limited Edition Sneakers</h3>
+      <p className="shoe-description">
         These low-profile sneakers are your perfect casual wear companion.
-        Featuring a durable rubber outer sole, they'll withstand everything the
-        weather can offer.
+        Featuring a durable rubber outer sole, they will withstand everything
+        the weather can offer.
       </p>
-      <span>$125.00</span>
+      <div className="price-container">
+        <span className="price">$125.00</span>
+        <span className="discount-percentage">50%</span>
+      </div>
+      <span>$250.00</span>
       <div>
         <button>-</button>
         <span>3</span>
